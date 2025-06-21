f67f3ee6147e72a90a8a1896632fc3e079af1b40
Julien Banchongphanith <jbanchon@f0r2s2.clusters.42paris.fr>
2025-06-21 12:02:22 +0200

    movements OK

--- srcs/cub_mlx/mlx_moves.c
+++ srcs/cub_mlx/mlx_moves.c
@@ -6,7 +6,7 @@
 /*   By: jbanchon <jbanchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:09:21 by jbanchon          #+#    #+#             */
-/*   Updated: 2025/06/21 11:55:45 by jbanchon         ###   ########.fr       */
+/*   Updated: 2025/06/21 12:02:00 by jbanchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,62 +24,6 @@ int	is_walkable(t_map *map, double x, double y)
 		return (0);
 	return (map->map[grid_y][grid_x] != '1');
 }
-
-// void	move_forward(t_map *map)
-// {
-// 	double	new_x;
-// 	double	new_y;
-
-// 	new_x = map->player_x + map->dir_x * MOVE_SPEED;
-// 	new_y = map->player_y + map->dir_y * MOVE_SPEED;
-// 	if (is_walkable(map, new_x, map->player_y))
-// 		map->player_x = new_x;
-// 	if (is_walkable(map, map->player_x, new_y))
-// 		map->player_y = new_y;
-// 	printf("Moved forward to (%.2f, %.2f)\n", map->player_x, map->player_y);
-// }
-
-// void	move_backward(t_map *map)
-// {
-// 	double	new_x;
-// 	double	new_y;
-
-// 	new_x = map->player_x - map->dir_x * MOVE_SPEED;
-// 	new_y = map->player_y - map->dir_y * MOVE_SPEED;
-// 	if (is_walkable(map, new_x, map->player_y))
-// 		map->player_x = new_x;
-// 	if (is_walkable(map, map->player_x, new_y))
-// 		map->player_y = new_y;
-// 	printf("Moved backward to (%.2f, %.2f)\n", map->player_x, map->player_y);
-// }
-
-// void	move_left(t_map *map)
-// {
-// 	double	new_x;
-// 	double	new_y;
-
-// 	new_x = map->player_x - map->plane_x * MOVE_SPEED;
-// 	new_y = map->player_y - map->plane_y * MOVE_SPEED;
-// 	if (is_walkable(map, new_x, map->player_y))
-// 		map->player_x = new_x;
-// 	if (is_walkable(map, map->player_x, new_y))
-// 		map->player_y = new_y;
-// 	printf("Moved left to (%.2f, %.2f)\n", map->player_x, map->player_y);
-// }
-
-// void	move_right(t_map *map)
-// {
-// 	double	new_x;
-// 	double	new_y;
-
-// 	new_x = map->player_x + map->plane_x * MOVE_SPEED;
-// 	new_y = map->player_y + map->plane_y * MOVE_SPEED;
-// 	if (is_walkable(map, new_x, map->player_y))
-// 		map->player_x = new_x;
-// 	if (is_walkable(map, map->player_x, new_y))
-// 		map->player_y = new_y;
-// 	printf("Moved right to (%.2f, %.2f)\n", map->player_x, map->player_y);
-// }
 
 void	rotate_left(t_map *map)
 {
@@ -116,10 +60,9 @@ void	rotate_right(t_map *map)
 	printf("Rotated left to (%.2f, %.2f)\n", map->player_x, map->player_y);
 }
 
-static void	calc_move_vector(t_map *map, double *v_x, double *v_y)
+static void	accumulate_vector(t_map *map, double *v_x, double *v_y)
 {
 	t_game	*game;
-	double	mag;
 
 	game = map->game;
 	*v_x = 0;
@@ -144,12 +87,24 @@ static void	calc_move_vector(t_map *map, double *v_x, double *v_y)
 		*v_x += map->plane_x * MOVE_SPEED;
 		*v_y += map->plane_y * MOVE_SPEED;
 	}
+}
+
+static void normalize_vector(double *v_x, double *v_y)
+{
+	double mag;
+
 	mag = sqrt((*v_x) * (*v_x) + (*v_y) * (*v_y));
 	if (mag > 0.0)
 	{
 		*v_x = (*v_x / mag) * MOVE_SPEED;
 		*v_y = (*v_y / mag) * MOVE_SPEED;
 	}
+}
+
+static void calc_move_vector(t_map *map, double *v_x, double *v_y)
+{
+	accumulate_vector(map, v_x, v_y);
+	normalize_vector(v_x, v_y);
 }
 
 static void	apply_move(t_map *map, double v_x, double v_y)
